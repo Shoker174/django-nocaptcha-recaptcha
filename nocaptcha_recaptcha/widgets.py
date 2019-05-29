489ebda7ba366ec9a8679c8efdb2585d47141e4d
azat.zakirov <azat.zakirov@redsolution.ru>
2019-05-29 19:07:43 +0500

    added js for rerender widget

--- nocaptcha_recaptcha/widgets.py
+++ nocaptcha_recaptcha/widgets.py
@@ -1,8 +1,29 @@
-from django import forms
-from django.conf import settings
 from django.utils.safestring import mark_safe
-
+from django.template.loader import render_to_string
+from django.utils.translation import get_language
+from django.conf import settings
+from django import forms
 from . import client
+
+
+def displayhtml(site_key, gtag_attrs, js_params, name):
+    """Gets the HTML to display for reCAPTCHA
+
+    site_key -- The public api key provided by Google ReCaptcha
+    """
+
+    if 'hl' not in js_params:
+        js_params['hl'] = get_language()[:2]
+
+    return render_to_string(
+        client.WIDGET_TEMPLATE,
+        {
+            'fallback_url': client.FALLBACK_URL,
+            'site_key': site_key,
+            'js_params': js_params,
+            'gtag_attrs': gtag_attrs,
+            'name': name
+        })
 
 
 class NoReCaptchaWidget(forms.widgets.Widget):
@@ -17,8 +38,7 @@ class NoReCaptchaWidget(forms.widgets.Widget):
         self.js_params = js_params
 
     def render(self, name, value, gtag_attrs=None, **kwargs):
-        return mark_safe(u'%s' % client.displayhtml(
-            self.site_key, self.gtag_attrs, self.js_params))
+        return mark_safe(u'%s' % displayhtml(self.site_key, self.gtag_attrs, self.js_params, name))
 
     def value_from_datadict(self, data, files, name):
         return data.get(self.g_nocaptcha_response, None)
